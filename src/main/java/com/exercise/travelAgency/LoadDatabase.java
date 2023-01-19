3f26a5f6e6f1f2640743bc726b0fdb3e7dac5c9e
rodrikk <rodrik33.2002@gmail.com>
2023-01-19 12:58:29 +0100

    Removed unused import

--- src/main/java/com/exercise/travelAgency/LoadDatabase.java
+++ src/main/java/com/exercise/travelAgency/LoadDatabase.java
@@ -11,8 +11,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 @Configuration
 class LoadDatabase {
 
