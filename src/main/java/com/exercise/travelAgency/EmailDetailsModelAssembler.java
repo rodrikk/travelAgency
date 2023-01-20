ea3a81c5b5a880ac0b4dd337b55c20671eb08f8e
rodrikk <rodrik33.2002@gmail.com>
2023-01-20 13:08:05 +0100

    Email details persistency addded

---
+++ src/main/java/com/exercise/travelAgency/EmailDetailsModelAssembler.java
@@ -0,0 +1,17 @@
+package com.exercise.travelAgency;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+@Component
+public class EmailDetailsModelAssembler implements RepresentationModelAssembler<EmailDetails, EntityModel<EmailDetails>> {
+    @Override
+    public EntityModel<EmailDetails> toModel(EmailDetails entity) {
+        return EntityModel.of(entity, //
+                linkTo(methodOn(EmailServiceController.class).one(entity.getId())).withSelfRel(),
+                linkTo(methodOn(EmailServiceController.class).all()).withRel("emails"));
+    }
+}
+
